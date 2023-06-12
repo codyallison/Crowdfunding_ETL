f262a933336e20371f03154bc5c1fa83c382fdd7
codyallison <cody.allison26@gmail.com>
2023-06-12 14:30:55 -0500

    finished contacts and sql portion

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/nu3XdA
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "contacts" (
+    "contact_id" INTEGER UNIQUE  NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10) UNIQUE  NOT NULL,
+    "category" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10) UNIQUE  NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INTEGER UNIQUE NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR(20)   NOT NULL,
+    "backers_count" integer   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launch_data" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "staff_pick" boolean   NOT NULL,
+    "spotlight" boolean   NOT NULL,
+    "category_id" VARCHAR(20)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"),
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
---
+++ select_validations.sql
@@ -0,0 +1,8 @@
+-- validate each database for correct population of data:
+select * from campaign;
+
+select * from contacts;
+
+select * from category;
+
+select * from subcategory;
